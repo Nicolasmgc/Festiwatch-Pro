35df7f12b9c6784841a345305a17741247baa576
conspersad <117267836+conspersad@users.noreply.github.com>
2022-12-05 09:10:01 +0100

    test css

--- includes/monprofil.php
+++ includes/monprofil.php
@@ -3,15 +3,27 @@ session_start();
 if(isset($_SESSION['email']) && (isset($_SESSION['nom'])))
 {
     ?>
-    
-    <h1>Bienvenue sur votre profil</h1>
-    <p>Votre email : <?php echo $_SESSION['email']; ?> </p>
-    <p>Votre id : <?php echo $_SESSION['id']; ?> </p>
-    <p>Votre nom : <?php echo $_SESSION['nom']; ?> </p>
-    <p>Votre prénom : <?php echo $_SESSION['prenom']; ?> </p>
-    <p>Votre numéro de téléphone : <?php echo $_SESSION['numtelephone']; ?> </p>
-    <p>Votre adresse : <?php echo $_SESSION['adresse']; ?> </p>
-    <p>Votre ville : <?php echo $_SESSION['ville']; ?> </p>
+         <!DOCTYPE html>
+     <html>
+        <head>
+           <link rel="stylesheet" href="Login.css">
+           <h1>Bienvenue sur votre profil</h1>
+         </head>
+     <body> 
+     <div class="round"> 
+     <p>Votre email </p>
+     <?php echo $_SESSION['email']; ?> 
+     <p>Votre nom </p>
+     <?php echo $_SESSION['nom']; ?>
+     <p>Votre prénom</p> 
+    <?php echo $_SESSION['prenom']; ?> 
+    <p>Votre numéro de téléphone </p>
+    <?php echo $_SESSION['numtelephone']; ?> 
+    <p>Votre adresse </p>
+     <?php echo $_SESSION['adresse']; ?> 
+    <p>Votre ville </p>
+     <?php echo $_SESSION['ville']; ?> 
+    </div>
 
 
     <a href="deconnexion.php">Déconnexion</a>
@@ -21,6 +33,11 @@ if(isset($_SESSION['email']) && (isset($_SESSION['nom'])))
     <a href="..\index.php">Page d'accueil</a>
 
     <a href="festivals.php">Liste des festivals</a>
+    
+</body>
+</html>
+
+    
     
     <?php
 
