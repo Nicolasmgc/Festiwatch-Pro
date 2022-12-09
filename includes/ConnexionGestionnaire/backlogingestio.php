85f383f8dd96a2e2b10987b7bec62f75cb935741
BastienMaupas <bab.maupas@gmail.com>
2022-12-09 09:55:31 +0100

    login gestio qui marche

---
+++ includes/ConnexionGestionnaire/backlogingestio.php
@@ -0,0 +1,44 @@
+<?php
+
+    if(isset($_POST['formlogingestio']))
+    {
+        extract($_POST);
+
+        if(!empty($lfest_nom) && !empty($fpassword))
+        {
+
+            $q2 = $db->prepare("Select * FROM festival WHERE Fest_nom = :Fest_nom");
+            $q2->execute(['Fest_nom' => $lfest_nom]);
+            $result = $q2->fetch();
+
+            if($result == true)
+            {
+
+                $hashpasswordf = $result['Fest_password'];
+                if(password_verify($fpassword, $hashpasswordf))
+                {
+                    echo "Le mot de passe est bon, connexion en cours";
+                    
+                    $_SESSION['Fest_id'] = $result['Fest_id'];
+                    $_SESSION['Fest_nom'] = $result['Fest_nom'];
+                    $_SESSION['Fest_password'] = $result['Fest_password'];
+                }
+                else
+                {
+                    echo "Le mot de passe n'est pas correcte";
+                }
+            }
+            else
+            {
+                echo "Le festival ayant comme nom " . $lfest_nom . " n'existe pas";
+            }
+
+        }
+        else
+        {
+            echo "Veuillez compléter l'ensemble des champs";
+        }
+
+    }
+
+?>
---
+++ includes/ConnexionGestionnaire/logingestio.php
@@ -0,0 +1,52 @@
+<?php session_start();
+
+?>
+
+
+<?php 
+if(isset($_SESSION['Fest_nom']))
+{
+    ?>
+    <h1>Bienvenue sur votre profil</h1>
+    <p>Votre nom : <?php echo $_SESSION['Fest_nom']; ?> </p>
+
+    <?php
+
+}else{
+    echo "Veuillez vous connecter à votre compte";
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="Login2.css">
+<head>
+    <title>Login</title>
+
+</head>
+
+<?php include 'C:\wamp64\www\includes\database.php';
+global $db;
+?>
+
+<body>
+
+<form method="post">
+    <input type="texte" name="lfest_nom" id="lfest_nom" placeholder="Le nom de votre festival" required><br/>
+    <input type="password" name="fpassword" id="fpassword" placeholder="Votre Mot de passe" required><br/>
+    <button><input type="submit" name="formlogingestio" id="formlogingestio" value="Ok"></button><br/>
+</form>
+
+    <?php include 'backlogingestio.php'; ?>
+
+    <a href="./Page d'accueil/index.php" class="acc"> Revenir à la page d'accueil </a>
+    
+    
+
+    </div>
+
+</body>
+    
+</html>
