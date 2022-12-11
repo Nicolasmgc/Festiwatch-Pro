d04e4470f664c7adbf7f871119af8fbd1644079a
conspersad <117267836+conspersad@users.noreply.github.com>
2022-12-11 15:32:07 +0100

    costance

--- includes/Page d'accueil/AProposDeNous/A_propos_de_nous.php
+++ includes/Page d'accueil/AProposDeNous/A_propos_de_nous.php
@@ -93,7 +93,7 @@
 
             <div class="lien">
             <a href="https://www.instagram.com/pro_detech">Condition général d'utilisation</a>
-            <a href="https://www.instagram.com/pro_detech"> FAQ</a>
+            <a href="faq.php"> FAQ</a>
             <a href="login1.php">Connexion</a>
             </div>
 
