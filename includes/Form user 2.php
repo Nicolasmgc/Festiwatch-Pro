cda16b807de4d99cdb98bf01e9a26665d1472f2d
BastienMaupas <bab.maupas@gmail.com>
2022-12-15 23:37:08 +0100

    form user 2 accès au festival user

--- includes/Form user 2.php
+++ includes/Form user 2.php
@@ -11,6 +11,29 @@ session_start();
         <link rel="stylesheet" type="text/css" href="styleformuser2.css">
     </head>
     <body>
+    <h1>Bienvenue sur la page du festival <?php // echo $_GET['Fest_nom']?></h1>
+    <div id="searchBox">
+                    <input id="searchBar"/>
+                    <a href=""><img id="searchIcon" src="searchIcon.png" alt="search"></a>
+                </div>
+    <div class="global">            
         <img href="./Efrontech/Form user 2.html"  src="les_ardentes_2022.jpg">
+        <div class="round"> 
+    <p>Votre email : </p>
+     <p><?php echo $_SESSION['email']; ?> </p>
+     <p>Votre nom :</p>
+    <p> <?php echo $_SESSION['nom']; ?></p>
+     <p>Votre prénom :</p> 
+    <p><?php echo $_SESSION['prenom']; ?> </p>
+    <p>Votre numéro de téléphone : </p>
+    <p><?php echo $_SESSION['numtelephone']; ?> </p>
+    <p>Votre adresse : </p>
+     <p><?php echo $_SESSION['adresse']; ?> </p>
+    <p>Votre ville : </p>
+     <p><?php echo $_SESSION['ville']; ?> </p>
+    </div>
+</div>
+
+                
     </body>
 </html>
