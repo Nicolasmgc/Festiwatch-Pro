55e189f3b62473c481451385849b61ab2d059ec6
conspersad <117267836+conspersad@users.noreply.github.com>
2022-12-15 22:24:34 +0100

    footer

--- includes/User/UserData/userData.php
+++ includes/User/UserData/userData.php
@@ -77,5 +77,26 @@
         </div>
 
     </body>
+    <footer>
+           <div class="contact">
+           <br>
+            Nous contacter <br>
+            Mail:<br> prodetec@gmail.com <br>
+            Numéro:<br> 068975412 <br>
+            Adresse: <br>10 Rue de Vanves, 92130 Issy-les-Moulineaux
+            </div>
+         
+            <div class="foot">
+            <div class="reseaux">
+            <a href="https://www.instagram.com/pro_detech"> <img src="insta.png" alt="icone de insta" height="60"></a>
+            <img src="youtube.png" alt="icone de insta" height="70">
+            <img src="twitter.png" alt="icone de insta" height="75"  class="twitter">
+            </div>
+            <div class="lien">
+            <a href="../../cgu.php">Conditions général d'utilisation</a>
+            <a href=".././FAQ/faq.php"> FAQ</a>
+            <a href="../login1.php">Connexion</a>
+            </div> </div>
+             </footer>
 
 </html>
