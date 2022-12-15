2ae1220edd99035f45a9cf76a9bfcf443ffb44ba
Nicolasmgc <80405053+Nicolasmgc@users.noreply.github.com>
2022-12-16 00:26:37 +0100

    headeralerte

--- includes/alertes.php
+++ includes/alertes.php
@@ -13,6 +13,58 @@ session_start();
 </head>
 
 <body>
+<nav>
+            <ul>
+               <li><img src="Logo alternatif2.png" class="logo" >  </a></li>   
+                  
+                <li><a href="#" > Accueil </a></li>
+                <li><a href="./FAQ/faq.php"> FAQ </a></li>
+                <li><a href="./AProposDeNous/A_propos_de_nous.php"> A propos de nous </a></li>
+                
+                <li class="deroulant"><?php if(isset($_SESSION['email'])){
+                            ?>
+                        
+                      <a><?php echo $_SESSION['email'];?></a>
+                        <ul class="sous">
+                            <li><a href="../monprofil.php"> Voir mon profil </a></li>
+                            <li><a href="../deconnexion.php"> Se déconnecter </a></li>
+                        </ul>           
+                        
+
+
+                        <?php
+                        }
+                        
+                        elseif(isset($_SESSION['Fest_id'])){
+                            ?>
+
+
+                        <a><?php echo $_SESSION['Fest_nom'];?></a>
+                        <ul class="sous">
+                            <li><a href="../ConnexionGestionnaire/mesinfos.php?Fest_id=".$_SESSION['Fest_id']> Voir mes infos </a></li> <?php // Truc très ghetto ça marche moyennement ce href faire gaffe pendant la démo ?>
+                            <li><a href="../deconnexion.php"> Se déconnecter </a></li>
+                        </ul>  
+
+
+                            <?php
+                        }
+                        
+                        else{ ?>
+                        <li><a href="../login1.php">Se connecter </a></li>
+                        
+                        <?php } ?></a>
+                          
+                    
+                    
+                    
+                </li>
+                
+                
+                
+                
+            </ul>
+        </nav>
+
     <h1> Les alertes </h1>
     <p> Vous êtes sur la page du festival "Les ardentes" </p>
 <div class= page> <!-- C'est pour naviguer entre les pages de différents festivals -->
