134e6bdb5db3d8373f54599d8ba99f62ccce449f
conspersad <117267836+conspersad@users.noreply.github.com>
2023-01-18 21:49:52 +0100

    foot

--- includes/Vue/Vue/monprofiluser/monprofil.php
+++ includes/Vue/Vue/monprofiluser/monprofil.php
@@ -197,8 +197,8 @@ if(isset($_POST['formupdate3'])){
          
             <div class="foot">
             <div class="reseaux">
-            <a href="https://www.instagram.com/pro_detech"> <img src="../../../PNG/insta.png" alt="icone de insta"height="65"  ></a>
-            <img src="../../../PNG/youtube.png" alt="icone de insta" height="65" >
+            <a href="https://www.instagram.com/pro_detech"> <img src="../../../PNG/insta.png" alt="icone de insta" height="68"></a>
+            <img src="../../../PNG/youtube.png" alt="icone de insta" height="70" width="60">
             <img src="../../../PNG/twitter.png" alt="icone de insta" height="75">
             </div>
             <div class="lien">
