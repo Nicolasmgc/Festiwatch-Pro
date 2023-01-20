f2008d25bb1bd0d60af6d07f6116bf8eca0bd18b
Nicolasmgc <80405053+Nicolasmgc@users.noreply.github.com>
2023-01-20 14:06:43 +0100

    accueiladmin

--- includes/Vue/Vue/paneladmin/accueiladmin.php
+++ includes/Vue/Vue/paneladmin/accueiladmin.php
@@ -7,8 +7,85 @@ if(isset($_SESSION['role_id'])){
     if($_SESSION['role_id'] == 2){
 ?>
 
+<html>
+    <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="cssaccueiladmin.css">
+    </head>
+<body>
+<nav>
+            <ul>
+                <li><img src="../../../PNG/Logo alternatif.png" class="logo"></li> 
+                <li><a href="../Pagedaccueil/index.php" > Accueil </a></li>
+                <li><a href="../FAQ/faq.php"> FAQ </a></li>
+                <li><a href="../Apropos/A_propos_de_nous.php"> À propos de nous </a></li>
+                
+                
+                
+                <li class="deroulant">
+                    <?php if(isset($_SESSION['email'])){
+                            ?>
+                        
+                      <a><?php echo $_SESSION['email'];?></a>
+                    <ul class="sous">
+                        <li><a href="../monprofiluser/monprofil.php"> Voir mon profil </a></li>
+                        <li><a href="#"> Liste des festivals </a></li>
+                        <li><a href="../../../Controller/deconnexion.php"> Se déconnecter </a></li>
+                        <?php if($_SESSION['role_id'] == '2'){
+                        ?>
+                        
+                            <li><a href="../paneladmin/accueiladmin.php"> Pannel Admin </a></li>
+                            <?php } ?>
+                        
+                        </ul>
+                                               
+                        <?php
+                }
+                        elseif(isset($_SESSION['Fest_id'])){
+                            ?>
+
+
+                        <a><?php echo $_SESSION['Fest_nom'];?></a>
+                        <ul class="sous">
+                            <li><a href="../mesinfosgestio/mesinfos.php"> Voir mes infos </a></li> <?php // Truc très ghetto ça marche moyennement ce href faire gaffe pendant la démo ?>
+                            <li><a href="../../../Controller/deconnexion.php"> Se déconnecter </a></li>
+                        </ul>  
+
+
+                            <?php
+                        }
+                        else{ ?>
+                        <li><a href="../Connexionuser/login1.php">Se connecter </a></li>
+                        
+                        <?php } ?>
+                    
+                    
+                    
+                </li>
+                
+                
+                
+                
+            </ul>
+        </nav>
+
+
+
 <a href="gestionuser.php">Gérer les utilisateurs</a>
 <a href="gestiongestio.php">Gérer les gestionnaires</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <?php
     }else{
